ffff57f70e40d4db9132114516d9b11f472acbc9
Victor Henrique <victorhenriqu3@outlook.com>
2021-12-08 22:31:48 -0400

    1- Parte 2 do Desafio(Estrutura Inicial de Classes)

---
+++ DIO.Bank/Conta/Conta.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+    }
+}
